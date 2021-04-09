c264cfd2808eddc38aa96bfabc8edeccccc215f8
Simon Stone <simon.stone@tu-dresden.de>
2021-04-09 14:10:24 +0200

    Correcte female vocal tract shapes Added code to plot transfer functions calculated with 1d method.

--- transfer-functions/PlotTransferFunctions.m
+++ transfer-functions/PlotTransferFunctions.m
@@ -7,54 +7,100 @@ addpath('../include')
 fc_hi = 5e3;  % Upper cutoff frequency for transfer function display
 
 % Input
-tf_files = dir('multimodal/*.txt');
+tf_mm_files = dir('multimodal/*.txt');
+tf_vtl_files = dir('1d/*.txt');
 
 % Output
 save_formants = true;
-formantFilepath_male = 'multimodal/m__formants_MM.csv';
-formantFilepath_female = 'multimodal/f__formants_MM.csv';
+mm_formantFilepath_male = 'multimodal/m__formants_MM.csv';
+mm_formantFilepath_female = 'multimodal/f__formants_MM.csv';
+vtl_formantFilepath_male = '1d/m__formants_1d.csv';
+vtl_formantFilepath_female = '1d/f__formants_1d.csv';
 
+%%
+sounds = ["a", "e", "i", "o", "u"];
+gender = ["m", "f"];
 
-
-%% 
-formants_male = table('Size', [0, 5], ...
+%% Baseline VTL
+vtl_formants_male = table('Size', [0, 5], ...
 'VariableTypes', {'string', 'double', 'double', 'double', 'double'}, ...
 'VariableNames', {'sound', 'F1', 'F2', 'F3', 'F4'});
-formants_female = formants_male;
+vtl_formants_female = vtl_formants_male;
+
+for file = tf_vtl_files' 
+    tf = readtable(fullfile(file.folder, file.name));
+    tf.Properties.VariableNames = {'f_Hz', 'mag', 'phi'};    
+    
+    tokens = split(file.name, '_');
+    %% Plot transfer function and identify formants
+    % Plot male speaker in even figures and female in odd
+    figureIdx = (2*find(sounds == tokens{2}))-1 + find(gender == tokens{1}) - 1;
+    figure(figureIdx);     
+    [pks, F] = plotTransferFunctionAndFindFormants(tf, fc_hi);
+    h = get(gca, 'Children');
+    set(h(end), 'LineStyle', '--');
+    title([tokens{1}, ', /', tokens{2}, '/'], 'interpreter', 'none');
+    
+    % Add first four formant values to table (one table for male, one file
+    % for female    
+    if tokens{1} == 'm'
+        vtl_formants_male = [vtl_formants_male; {tokens{2}, F(1), F(2), F(3), F(4)}];
+    elseif tokens{1} == 'f'
+        vtl_formants_female = [vtl_formants_female; {tokens{2}, F(1), F(2), F(3), F(4)}];
+    end      
+end
 
 
-fileIdx = 1;
-for file = tf_files'
+
+%% Multi-modal method
+mm_formants_male = table('Size', [0, 5], ...
+'VariableTypes', {'string', 'double', 'double', 'double', 'double'}, ...
+'VariableNames', {'sound', 'F1', 'F2', 'F3', 'F4'});
+mm_formants_female = mm_formants_male;
+
+for file = tf_mm_files'
     tf = readtable(fullfile(file.folder, file.name), 'FileType', 'text', 'HeaderLines', 1);
     % The first row contains garbage with inconsistent formatting
     tf.Properties.VariableNames = {'f_Hz', 'mag', 'phi'};    
     
-    % Plot transfer function and identify formants
-    figure(fileIdx);    
-    [pks, F] = findpeaks(db(tf.mag(tf.f_Hz < fc_hi)), tf.f_Hz(tf.f_Hz < fc_hi));
-    findpeaks(db(tf.mag(tf.f_Hz < fc_hi)), tf.f_Hz(tf.f_Hz < fc_hi));
-    text(F, pks + 3, num2str(F))
-    xlabel('Frequency [Hz]');
-    ylabel('Magnitude [dB]');
-    title(file.name, 'interpreter', 'none');
-    fileIdx = fileIdx + 1;
-    
-    % Add first four formant values to table (one table for male, one file
-    % for female
+    %% Plot transfer function and identify formants
+    % Plot male speaker in even figures and female in odd
     tokens = split(file.name, '_');
+    figureIdx = (2*find(sounds == tokens{2}))-1 + find(gender == tokens{1}) - 1;
+    figure(figureIdx);     
+    hold on;
+    [pks, F] = plotTransferFunctionAndFindFormants(tf, fc_hi);
+    hold off;    
+    title([tokens{1}, ', /', tokens{2}, '/'], 'interpreter', 'none');
+    % Add first four formant values to table (one table for male, one file
+    % for female    
     if tokens{1} == 'm'
-        formants_male = [formants_male; {tokens{2}, F(1), F(2), F(3), F(4)}];
+        mm_formants_male = [mm_formants_male; {tokens{2}, F(1), F(2), F(3), F(4)}];
     elseif tokens{1} == 'f'
-        formants_female = [formants_female; {tokens{2}, F(1), F(2), F(3), F(4)}];
-    end      
+        mm_formants_female = [mm_formants_female; {tokens{2}, F(1), F(2), F(3), F(4)}];
+    end 
+    
+    %% Add legend
+    h = get(gca, 'Children');
+    legend(findobj(h, 'Tag', 'Signal'), {'1d', 'multimodal'}, 'Location', 'southeast');
+    
 end
+
+
 
 %% Write formants to file
 if save_formants
     disp('Writing formants to file...')
-    writetable(formants_male, formantFilepath_male, 'Delimiter',' ');
-    writetable(formants_female, formantFilepath_female, 'Delimiter',' ');
+    writetable(mm_formants_male, mm_formantFilepath_male, 'Delimiter',' ');
+    writetable(mm_formants_female, mm_formantFilepath_female, 'Delimiter',' ');
     disp('...done.')
 end
 
-
+%%
+function [pks, F] = plotTransferFunctionAndFindFormants(tf, fc_hi)
+   [pks, F] = findpeaks(db(tf.mag(tf.f_Hz < fc_hi)), tf.f_Hz(tf.f_Hz < fc_hi));
+    findpeaks(db(tf.mag(tf.f_Hz < fc_hi)), tf.f_Hz(tf.f_Hz < fc_hi));
+    text(F, pks + 3, num2str(F))
+    xlabel('Frequency [Hz]');
+    ylabel('Magnitude [dB]');
+end
